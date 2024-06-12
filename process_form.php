c7b0e2060924a47bbb40b5c8e24f333a76e03d57
LucCo99 <66907111+LucCo99@users.noreply.github.com>
2024-06-12 15:58:57 +1000

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -17,14 +17,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.email';  // Specify your SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'coluc8440@gmail.com'; // Your SMTP username
-    $mail->Password = "liyycloibjqcnvhb"; // Your SMTP password
+    $mail->Username = ''; // Your SMTP username
+    $mail->Password = ""; // Your SMTP password
     $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, 'ssl' also accepted
     $mail->Port = 465; // TCP port to connect to
 
     // Set sender and recipient
-    $mail->setFrom("coluc8440@gmail.com", $name);
-    $mail->addAddress('coluc8440@gmail.com'); // Recipient email address
+    $mail->setFrom("", $name);
+    $mail->addAddress(''); // Recipient email address
 
     // Email content
     $mail->isHTML(false); // Set email format to plain text
